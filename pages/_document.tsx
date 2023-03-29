7213e8c0d6e87631e5ec27d61e20b12feb4e1750
Timofei Akylveren <akylverentimofei@mail.ru>
2023-03-29 18:23:46 +0300

    _document

---
+++ pages/_document.tsx
@@ -0,0 +1,21 @@
+// @ts-nocheck
+import { Html, Head, Main, NextScript } from 'next/document'
+import React from "react";
+
+export default function Document() {
+
+  return (
+    <Html>
+      <Head>
+        <meta name={"viewport"} content={"width=device-width, initial-scale=1"} />
+        <meta httpEquiv={'Content-Type'} content={"text/html; charset=utf-8"} />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="robots" content="index, follow" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
