ae8d3fe7963801f7941c52a7ae9e0aa68b83fb60
Timofei Akylveren <akylverentimofei@mail.ru>
2023-04-03 20:43:38 +0300

    card story

--- .storybook/stories/Card.stories.tsx
+++ .storybook/stories/Card.stories.tsx
@@ -3,7 +3,7 @@ import { Meta, Story } from "@storybook/react";
 import Card, {ICardProps} from "@/components/Card";
 
 export default {
-  title: 'UI/Card',
+  title: 'Card',
   component: Card,
   parameters: {
     backgrounds: {
