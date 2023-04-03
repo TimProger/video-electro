ee80aa9ebae05f051ecd32f8ba5adfdece819bd6
Timofei Akylveren <akylverentimofei@mail.ru>
2023-04-03 19:37:42 +0300

    dropdown story

---
+++ .storybook/stories/Dropdown.stories.tsx
@@ -0,0 +1,31 @@
+import * as React from "react";
+import { Meta, Story } from "@storybook/react";
+import Dropdown, {IDropdownProps} from "@/components/UI/Dropdown";
+import {useState} from "@storybook/store";
+
+export default {
+  title: 'Dropdown',
+  component: Dropdown,
+} as Meta;
+
+export const Default: Story<IDropdownProps> = () => {
+
+  const [open, setOpen] = useState(false)
+
+  const args = {
+    title: 'Какие бренды продаются на вашем сайте?',
+    text: 'Все виды выполняемых нами электромонтажных работ лицензированы. Потенциал компании позволяет реализовывать проекты любой сложности: от небольших частных заказов до крупных объектов в промышленной сфере до предоставления комплексного решения заказчику. В нашем активе более 250 крупных проектов, среди которых международный деловой центр «Москва-Сити».\n' +
+      'Окно автосклейлиться относительно количетсва текста!!!',
+    open,
+    onClick: () => {
+      setOpen(prev => !prev)
+    },
+    setDropdowns: () => {
+      setOpen(false)
+    }
+  }
+
+  return (
+    <Dropdown {...args} />
+  );
+}
