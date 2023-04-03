8074df998ac712def0d25e4d8df3e8cb3daa0815
Timofei Akylveren <akylverentimofei@mail.ru>
2023-04-03 18:05:39 +0300

    storybook button complete

--- .storybook/stories/Button.stories.tsx
+++ .storybook/stories/Button.stories.tsx
@@ -9,19 +9,38 @@ export default {
 export const Default: Story<IButtonProps> = (args) => <Button {...args} />;
 Default.args = {
   children: 'Click me',
-  onClick: () => {}
+  size: 'medium',
+  onClick: () => {
+    console.log('Default click')
+  }
 };
 
 export const Success: Story<IButtonProps> = (args) => <Button {...args} />;
 Success.args = {
   children: 'Click me',
   success: true,
-  onClick: () => {}
+  size: 'medium',
+  onClick: () => {
+    console.log('Success click')
+  }
 };
 
 export const Error: Story<IButtonProps> = (args) => <Button {...args} />;
 Error.args = {
   children: 'Click me',
   error: true,
-  onClick: () => {}
+  size: 'medium',
+  onClick: () => {
+    console.log('Error click')
+  }
+};
+
+export const Disabled: Story<IButtonProps> = (args) => <Button {...args} />;
+Disabled.args = {
+  children: 'Click me',
+  disabled: true,
+  size: 'medium',
+  onClick: () => {
+    console.log('Disabled click')
+  }
 };
