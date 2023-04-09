b7f3dd37c6b5ece3f7770364d71f162b8621c45e
Timofei Akylveren <akylverentimofei@mail.ru>
2023-04-10 01:24:45 +0300

    БЭМ select & dropdown

--- components/UI/Dropdown.tsx
+++ components/UI/Dropdown.tsx
@@ -20,14 +20,14 @@ const Dropdown: React.FC<IDropdownProps> = ({setDropdowns, title, text, open, on
   });
 
   return (
-    <div ref={ref} className={classNames(s.dropdown, {[s.dropdown__active]: open})}>
+    <div ref={ref} className={classNames(s.dropdown, {[s.dropdown_active]: open})}>
       <div onClick={onClick} className={s.dropdown__header}>
         <Text size={'medium'}>{title}</Text>
         <svg style={{transition: 'all .3s linear', transform: open ? 'rotate(180deg)' : 'rotate(0deg)'}} width="18" height="10" viewBox="0 0 18 10" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path d="M1 1L9 9L17 1" stroke="#5B74F9" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
         </svg>
       </div>
-      <div className={classNames(s.dropdown__text, {[s.dropdown__text__active]: open})}>
+      <div className={classNames(s.dropdown__text, {[s.dropdown__text_active]: open})}>
         <Text>{text}</Text>
       </div>
     </div>
--- components/UI/Select.tsx
+++ components/UI/Select.tsx
@@ -21,20 +21,20 @@ const Select: React.FC<ISelectProps> = ({sort, value, values, onClick}) => {
   });
 
   return (
-    <div ref={ref} className={classNames(s.select, {[s.select__active]: open})}>
+    <div ref={ref} className={classNames(s.select, {[s.select_active]: open})}>
       <div onClick={()=>setOpen(prev => !prev)} className={s.select__value}>
         <Text size={'small'}>{sort ? `${value.charAt(0).toUpperCase() + value.slice(1)}` : value}</Text>
         <svg style={{transition: 'all .3s linear', transform: open ? 'rotate(180deg)' : 'rotate(0deg)'}} width="18" height="10" viewBox="0 0 18 10" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path d="M1 1L9 9L17 1" stroke="#5B74F9" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
         </svg>
       </div>
-      <div className={classNames(s.select__values, {[s.select__values__active]: open})}>
+      <div className={classNames(s.select__values, {[s.select__values_active]: open})}>
         <div className={s.select__values__space}></div>
         {values.map((el)=>{
           return <div onClick={()=>{
             onClick(el)
             setOpen(false)
-          }} className={classNames(s.select__values__block, {[s.select__values__block__active]: el === value})}>
+          }} className={classNames(s.select__values__block, {[s.select__values__block_active]: el === value})}>
             <Text>{sort ? `По ${el}` : el}</Text>
           </div>
         })}
