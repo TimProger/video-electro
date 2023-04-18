5690bd9883dffe0f79b816a4a59961e941799f8f
Timofei Akylveren <akylverentimofei@mail.ru>
2023-04-18 14:30:15 +0300

    recovery

--- pages/recovery.tsx
+++ pages/recovery.tsx
@@ -6,7 +6,11 @@ import Container from '@/components/UI/Container';
 import Head from "next/head";
 import s from '@/styles/pages/Recovery.module.scss'
 import about from "@/public/images/pages/main/about.jpg"
+import recovery_one from "@/public/images/pages/recovery/1.png"
+import recovery_two from "@/public/images/pages/recovery/2.png"
+import recovery_three from "@/public/images/pages/recovery/3.png"
 import Image from "next/image";
+import classNames from "classnames";
 
 interface IRecoveryProps {
 }
@@ -34,7 +38,20 @@ const Recovery: React.FC<IRecoveryProps> = () => {
                 </div>
                 <div className={s.recovery__content__left__block}>
                   <Text type={'h2'} size={'big+'}>Этапы возврата</Text>
-                  <Text>Все виды выполняемых нами электромонтажных работ лицензированы. Потенциал компании позволяет реализовывать проекты любой сложности: от небольших частных заказов до крупных объектов в промышленной сфере до предоставления комплексного решения заказчику. В нашем активе более 250 крупных проектов, среди которых международный деловой центр «Москва-Сити».</Text>
+                  <div className={s.recovery__content__left__block__numbers}>
+                    <div className={s.recovery__content__left__block__numbers__container}>
+                      <Image unoptimized width={33} height={77} src={recovery_one.src} alt=""/>
+                      <Text>Оплатите ваш заказ, нажав на кнопку слева и проверьте статус в личном кабинете.</Text>
+                    </div>
+                    <div className={classNames(s.recovery__content__left__block__numbers__container)}>
+                      <Image unoptimized width={33} height={77} src={recovery_two.src} alt=""/>
+                      <Text>С вами свяжеться наш менеджер и заключит с вами договор.</Text>
+                    </div>
+                    <div className={s.recovery__content__left__block__numbers__container}>
+                      <Image unoptimized width={33} height={77} src={recovery_three.src} alt=""/>
+                      <Text>По согласованному времени, вы получите товары. Не забудьте оставить отзывы на товары. Спасибо, что выбрали нас!</Text>
+                    </div>
+                  </div>
                 </div>
               </div>
               <div className={s.recovery__content__right}>
